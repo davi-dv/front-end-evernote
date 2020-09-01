51b5b194c42a4db86022aba12399b19e0cde6a7c
Davi silva <davisilva@unochapeco.edu.br>
2020-09-01 16:46:45 -0300

    criado sidebar usando a lib react-burger-menu,incluido botao burguer no componente de header_logged e criado states para esse botao usando react-burger-menu

--- src/components/notes/index.js
+++ src/components/notes/index.js
@@ -1,13 +1,38 @@
-import react,{useState,Fragment} from 'React'
-import '../../styles/notes.scss'
+import React, { Fragment, useEffect, useState } from 'react';
+import { Column, Button } from "rbx";
+import "../../styles/notes.scss";
+import { push as Menu } from 'react-burger-menu'
 
 
-const Notes = ()=>{
-    return(
-        <Fragment>
-            notes..
-        </Fragment>
-    )
+function Notes(props) {
+
+  return (
+    <Fragment>
+      <Column.Group className="notes" id="notes">
+        <Menu
+          pageWrapId={"notes-editor"}
+          isOpen={props.isOpen}
+          onStateChange={(state) => props.setIsOpen(state.isOpen)}
+          disableAutoFocus
+          outerContainerId={"notes"}
+          customBurgerIcon={false}
+          customCrossIcon={false}
+        >
+          <Column.Group>
+            <Column size={10} offset={1}>
+              Search...
+            </Column>
+          </Column.Group>
+          <p>List...</p>
+        </Menu>
+
+
+        <Column size={12} className="notes-editor" id="notes-editor">
+          Editor...
+        </Column>
+      </Column.Group>
+    </Fragment>
+  )
 }
 
 export default Notes;
--- src/screens/notes/index.js
+++ src/screens/notes/index.js
@@ -1,12 +1,18 @@
-import React, { Fragment } from 'react';
+import React, { Fragment,useState } from 'react';
 import HeaderLogged from '../../components/header_logged'
+import { Column } from "rbx";
 import Notes from '../../components/notes'
+import "../../styles/notes.scss";
 
-const NotesScreen = () => (
+
+const NotesScreen = () => {
+ const [isOpen,setIsOpen] = useState(false)
+ return(
   <Fragment>
-    <HeaderLogged/>
-    <Notes/>
+     <HeaderLogged setIsOpen={setIsOpen}/>
+     <Notes isOpen={isOpen} setIsOpen={setIsOpen}/>
   </Fragment>
-);
+ )
+ };
 
 export default NotesScreen;
