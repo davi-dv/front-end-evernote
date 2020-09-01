97b133b28019c68c42d525c3fb2506763b51579c
Davi silva <davisilva@unochapeco.edu.br>
2020-09-01 10:55:13 -0300

    criado estrutura da tela de notes

---
+++ src/components/notes/index.js
@@ -0,0 +1,13 @@
+import react,{useState,Fragment} from 'React'
+import '../../styles/notes.scss'
+
+
+const Notes = ()=>{
+    return(
+        <Fragment>
+            notes..
+        </Fragment>
+    )
+}
+
+export default Notes;
